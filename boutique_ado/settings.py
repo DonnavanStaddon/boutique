2c23e45d7843494e7c31935e9cf646c993a6812f
Donnavan Stadd <donnystaddon@gmail.com>
2023-10-16 21:41:40 +0000

    Added sorting and product counts to prduct page

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-do#ubk(%71^stwumif27h!iipyb8$yr^*1x&en)j=gh387%@0(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-donnavanstaddo-boutique-lnetxha0vl3.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['8000-donnavanstaddo-boutique-nfood3abarj.ws-eu105.gitpod.io']
 
 
 # Application definition
